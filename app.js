24b977207daa5e984aeb4cd650b5471397c26710
Qiaozhi Lei <georgeleiqiaozhi@gmail.com>
2026-03-15 15:41:21 -0700

    Match inline date control height

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const STORAGE_KEY = "github-todo-sync-config";
 const THEME_KEY = "github-todo-theme";
 const TODOS_PATH = "todos.json";
-const APP_VERSION = "2026-03-15 16:14";
-const APP_COMMIT_MESSAGE = "Use inline date editors";
+const APP_VERSION = "2026-03-15 16:17";
+const APP_COMMIT_MESSAGE = "Match inline date control height";
 const TODO_STATUSES = ["progress", "backlog", "done"];
 
 const state = {
